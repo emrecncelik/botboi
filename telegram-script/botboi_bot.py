ee45073cacc51ae857314e6157662ee6a64bbe01
Algın <algin_arslan90@hotmail.com>
2021-02-10 23:19:58 +0300

    Too many changes to write down

--- telegram-script/botboi_bot.py
+++ telegram-script/botboi_bot.py
@@ -7,28 +7,14 @@ from decouple import config
 import sys
 from inspect import signature
 
-#Get absolute path to assets
-script_abs_path = os.path.dirname(__file__)
-assets_relative_path = "assets/"
-assets_abs_path = os.path.join(script_abs_path,assets_relative_path)
-
-#Load local environment configuration
-token = config("TELEGRAM_API_TOKEN")
-telegram_group_id = int(config("TELEGRAM_GROUP_ID"))
-authorized_user_dc_id = config("AUTHORIZED_USER_DC_ID").split(",")
-
-#Dictionary for matching between discord and telegram
-telegram_dc_filename = "telegram_dc_dictionary.pickle"
-telegram_dc_dictionary = {}
-
-#Init telegram bot
-bot = telebot.TeleBot(token,parse_mode=None)
-
-
 #Return the telegram discord dictionary
 def get_tg_dc_dictionary(file_name):
-    with open(assets_abs_path+file_name,"rb") as fp:
-        return pickle.load(fp)
+    try:
+        with open(assets_abs_path+file_name,"rb") as fp:
+            return pickle.load(fp)
+    except Exception as e:
+        sys.stderr.write(e)
+        return False
     
 #Saves dictionary data with pickle
 def save_tg_dc_dictionary(file_name):
@@ -37,20 +23,54 @@ def save_tg_dc_dictionary(file_name):
             pickle.dump(telegram_dc_dictionary,fp)
             return True
             
-    except:
+    except Exception as e:
+        sys.stderr.write(e)
         return False
 
+#Get absolute path to assets
+script_abs_path = os.path.dirname(__file__)
+assets_relative_path = "assets/"
+assets_abs_path = os.path.join(script_abs_path,assets_relative_path)
+
+#Load local environment configuration
+token = config("TELEGRAM_API_TOKEN")
+#telegram_group_id = int(config("TELEGRAM_DEV_CHAT_ID"))
+telegram_group_id = int(config("TELEGRAM_GROUP_ID"))
+authorized_user_dc_id = config("AUTHORIZED_USER_DC_ID").split(",")
+authorized_dev = config("AUTHORIZED_DEV_DC_ID").split(",")
+
+#Dictionary for matching between discord and telegram
+telegram_dc_filename = "telegram_dc_dictionary.pickle"
+telegram_dc_dictionary = {}
+
+#Load files
+if os.path.exists(os.path.join(assets_abs_path,telegram_dc_filename)):    
+    telegram_dc_dictionary = get_tg_dc_dictionary(telegram_dc_filename)
+
+#Init telegram bot
+bot = telebot.TeleBot(token,parse_mode=None)
 
 #Connect telegram_id and discord_id in dictionary, save new pair to file
 #Return True if added sucessfully, false if not
 def add_discord_telegram_pair(telegram_username,dc_id,file_name=telegram_dc_filename):
     
     try:
-        telegram_dc_dictionary[dc_id] = telegram_username
+        telegram_dc_dictionary[str(dc_id)] = str(telegram_username)
         result = save_tg_dc_dictionary(file_name)
         return result
     
-    except:
+    except Exception as e:
+        sys.stderr.write(e)
+        return False
+    
+def remove_discord_telegram_pair(dc_id,file_name=telegram_dc_filename):
+    try:
+        telegram_dc_dictionary.pop(str(dc_id))
+        result = save_tg_dc_dictionary(file_name)
+        return result
+        
+    except Exception as e:
+        sys.stderr.write(e)
         return False
     
 
@@ -59,20 +79,20 @@ def add_discord_telegram_pair(telegram_username,dc_id,file_name=telegram_dc_file
 def get_online_dc_users(message):
 	bot.reply_to(message, "Yalnız onu yanlış kodluyorsun kardeşim.")
     
-@bot.message_handler(commands=['eurydice'])
-def eurydice(message):
-    bot.reply_to(message,"Eurydice'ın söylediği şarkı bağlamada yazılıp çalınmış.Vauv" )
-
-
-#Tags the person(user_id) in the group(chat_id)
-def tag_person_in_chat(user_name,chat_id):
-    bot.send_message(chat_id,"@"+user_name)
     
-#Ultimate call for 5 adat
-def call_for_5_adat(dc_user_id,chat_id=telegram_group_id,telegram_dc_dictionary=telegram_dc_dictionary):
-    #Convert given dc_id to telegram user_name
-    tag_person_in_chat(telegram_dc_dictionary[dc_user_id],chat_id)
-    bot.send_message(chat_id,"Knights of round table await for 5 adat...")
+def tag_message_id(tg_id,chat_id,message):
+    bot.send_message(chat_id,message,parse_mode="MarkdownV2")
+    
+
+def call_for_5_adat(dc_user_id,dc_name,chat_id=telegram_group_id,local_dictionary=telegram_dc_dictionary):
+    try:
+        telegram_id = local_dictionary[str(dc_user_id)]
+        message = "["+ dc_name+"]"+"(tg://user?id="+telegram_id+")"+", knights of round table await for the 5th adat \.\.\."
+        tag_message_id(telegram_id,chat_id,message)
+    except Exception as e:
+        raise e
+        
+        
     
 def get_function_index(function_name):
     try:
@@ -82,18 +102,13 @@ def get_function_index(function_name):
         
         return None
     
-    except:
+    except Exception as e:
+        sys.stderr.write(e)
         return None
     
 #Hardcoded due to security reasons. Only reachable methods out of the program
 usable_methods_dc = [add_discord_telegram_pair,call_for_5_adat]
-methods_for_telegram = []
-
-
-#Load files
-if os.path.exists(telegram_dc_filename):    
-    telegram_dc_dictionary = get_tg_dc_dictionary(telegram_dc_filename)
-
+usable_methods_telegram = []
 
 argc = len(sys.argv)
 
@@ -104,12 +119,12 @@ if(argc > 1):
     
     #Get the function given from the list
     function_index = get_function_index(sys.argv[1])
-    print("Function index:"+str(function_index))
     
     #If not a callable function
     if(function_index < 0):
         print("Not a usable function. Try again, fail again, fail better.")
         sys.stdout.flush()
+        quit()
     
     else: 
         #Get function parameters
@@ -121,33 +136,40 @@ if(argc > 1):
         if(argc-3 != len(func_param)):
             print("Man of many arguments, or few.")
             sys.stdout.flush()
+            quit()
         
         #TODO: Give arguments dynamically regarding the number of parameters
         else:
             
+            #TODO: Check authorization distinctively for dev and user
             #Check authorization
             if(sys.argv[-1] in authorized_user_dc_id):
-            #try:
-                #If given 2 argument
-                if(argc == 5):
-                    usable_methods_dc[function_index](sys.argv[2],sys.argv[3])
-               
-                #Given 1 argument
-                elif(argc == 4):
-                    usable_methods_dc[function_index](sys.argv[2])
+                try:
+                    #If given 2 argument
+                    if(argc == 5):
+                        usable_methods_dc[function_index](sys.argv[2],sys.argv[3])
+                   
+                    #Given 1 argument
+                    elif(argc == 4):
+                        usable_methods_dc[function_index](sys.argv[2])
+                    
+                    print("Function ran sucessfully")
+                    sys.stdout.flush()
+                    quit()
                 
-                print("Function run sucessfully")
-                sys.stdout.flush()
-            
-            #except:
-                 #print("Something gone very teribly, function sucessfully failed")
-                 #sys.stdout.flush()
+                except Exception as e:
+                     print("Something gone terribly wrong. Try again, or maybe not")
+                     sys.stdout.flush()
+                     
+                     sys.stderr.write(e)
+                     quit()
                 
             #Authorization failed
             else:
                 print("Call me your manager")
                 sys.stdout.flush()
-    
+                quit()
+                
 
 #If not called with command line arguments
 elif(argc == 1):
