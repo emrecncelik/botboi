f556991ad9bee7c1fea3f628cfe71b10f43e1470
emrecan <emrecncelik@gmail.com>
2021-02-13 00:57:46 +0300

    feat: add delete command

--- cogs/dev.py
+++ cogs/dev.py
@@ -1,15 +1,26 @@
+import asyncio
 import discord
 from discord.ext import commands
 
+"""
+Commands:
+    -foo: obivious
+    -reload: reloads a cog, used to update
+             commands without closing the bot
+    -gtfo: closes bot, in a rude way
+    -delete: deletes given number of messages
+"""
 
 class dev(commands.Cog, name='dev'):
     def __init__(self, bot):
         self.bot = bot
 
+
     @commands.command(name='foo',
                       help='Replies to your foo-ish message in a foo-ish way')
     async def foo(self, context):
         await context.message.reply('Foo indeed.')
+
 
     @commands.command(name='reload',
                       aliases=['update'],
@@ -23,14 +34,33 @@ class dev(commands.Cog, name='dev'):
             except Exception as ex:
                 print(f'\t-{cog} not reloaded\n\t\t{ex}')
                 
+
     @commands.command(name='gtfo',
                       aliases=['bye', 'fuckoff', 'poweroff', 'close'],
                       help='Closes the bot')
     async def gtfo(self, context):
         print(f'Bot closed by {context.message.author}')
         await context.message.add_reaction('🖕')
-        await context.message.reply('Alright dude, chill. I\'m out.')
+        response = await context.message.\
+            reply('Alright dude, chill. I\'m out.')
+        await asyncio.sleep(3)
+        await response.delete()
         await self.bot.logout()
+
+
+    @commands.command(name='delete',
+                      aliases=['purge', 'clear'],
+                      help='Deletes given number of previous messages (max. 100)')
+    async def delete(self, context, num_messages: int):
+        channel = context.channel
+        await channel.purge(limit=num_messages+1,
+                            check=None,
+                            before=None)
+        response = await channel\
+            .send(f'Deleted {num_messages} message(s)')
+        await asyncio.sleep(3)
+        await response.delete()
+
 
 def setup(bot):
     bot.add_cog(dev(bot)) 
